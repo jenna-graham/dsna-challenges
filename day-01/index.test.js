919c8809d76b905a28e7689497907285f069fe74
jenna-graham <jclayto2@gmail.com>
2022-09-19 16:45:04 -0700

    Oddish test PRIM02

--- day-01/index.test.js
+++ day-01/index.test.js
@@ -2,14 +2,14 @@ const { reverseSentence, addCapitolLetter, oddOrEven } = require('./index');
 
 describe('primitive code challenges', () => {
   describe('reverseSentence', () => {
-    it('should reverse a sentence', () => {
+    it.only('should reverse a sentence', () => {
       expect(reverseSentence('alchemy rocks gold')).toBe('ymehcla skcor dlog');
     });
   });
 });
 describe('primitive code challenges', () => {
   describe('Add capitol letters to words', () => {
-    it('should add a capitol to first letter in each word', () => {
+    it.only('should add a capitol to first letter in each word', () => {
       expect(addCapitolLetter('alchemy ROCKS goLD')).toBe('Alchemy Rocks Gold');
     });
   });
@@ -19,6 +19,9 @@ describe('primitive code challenges', () => {
   describe('Determines if odd or even', () => {
     it.only('should determine oddness', () => {
       expect(oddOrEven(121)).toBe('Evenish');
+    });
+    it.only('should determines if even', () => {
+      expect(oddOrEven(41)).toBe('Oddish');
     });
   });
 });
