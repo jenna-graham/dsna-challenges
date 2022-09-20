c5d1161fd878bf756477cc9ff36aca9572291307
jenna-graham <jclayto2@gmail.com>
2022-09-20 16:10:51 -0700

    stack test remove item from array

--- day-02/tests/mytests.test.js
+++ day-02/tests/mytests.test.js
@@ -4,7 +4,13 @@ describe('Stack Tests', () => {
     it('Should add an item to an array', () => {
         const itemArray = new Stack([]);
         itemArray.classPush('fox');
-        expect(itemArray.readableArr).toEqual(['fox']);
+        itemArray.classPush('goose');
+        itemArray.classPush('lizard');
+        expect(itemArray.readableArr).toEqual(['fox', 'goose', 'lizard']);
     });
-  
+    it('pop should return the array without the', () => {
+        const itemArray = new Stack(['fox', 'goose', 'lizard']);
+        itemArray.classPop();
+        expect(itemArray.readableArr).toEqual(['fox', 'goose'])
+    })
 });
