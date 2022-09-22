7c3de527d3efd299d9661420b0b5726b18eed0ab
jenna-graham <jclayto2@gmail.com>
2022-09-22 16:42:49 -0700

    CH01 test passing

---
+++ day-03/BinaryTreeNode.js
@@ -0,0 +1,25 @@
+class BinaryTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+    add(node) {
+        if(node.value === this.value) {
+            this.value = node.value;
+        } else if (node.value <= this.value) {
+            if (this.right === null) {
+                this.right = node;
+            } else {
+                this.right.add(node);
+            }
+        }else if (node.value > this.value) {
+            if(this.left === null) {
+                this.left = node;
+            } else {
+                this.left.add(node);
+            }
+        }
+    }
+}
+module.exports = { BinaryTreeNode };
--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,5 +1,17 @@
+const { BinaryTreeNode } = require('./BinaryTreeNode');
 describe('node based data structures', () => {
   it('example test', () => {
     expect(1).toBe(1);
   });
+  it('creates a new tree', () => {
+    const B = new BinaryTreeNode('B');
+    const A = new BinaryTreeNode('A');
+    const C = new BinaryTreeNode('C');
+    B.add(A);
+    B.add(C)
+    console.log('A', A);
+    expect(B.value).toEqual('B');
+    expect(B.left.value).toEqual('C');
+  })
+
 });
